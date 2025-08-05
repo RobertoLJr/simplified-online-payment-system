0e65ced20b18be9162fd9d2f03b322b9ab33ca87
Roberto Junior <gsn.roberto@gmail.com>
2025-08-05 16:09:06 -0300

    build: initial setup

---
+++ src/main/java/com/robertoljr/sops/SimplifiedOnlinePaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.robertoljr.sops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedOnlinePaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedOnlinePaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robertoljr/sops/SimplifiedOnlinePaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertoljr.sops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedOnlinePaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
