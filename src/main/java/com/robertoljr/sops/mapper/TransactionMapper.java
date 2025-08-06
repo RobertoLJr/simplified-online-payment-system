1998185c0303ccbde21bbc7f1dfda589dffddf73
Roberto Junior <gsn.roberto@gmail.com>
2025-08-06 14:07:31 -0300

    feat: implement TransactionMapper

---
+++ src/main/java/com/robertoljr/sops/mapper/TransactionMapper.java
@@ -0,0 +1,34 @@
+package com.robertoljr.sops.mapper;
+
+import com.robertoljr.sops.dto.transaction.CreateTransactionDTO;
+import com.robertoljr.sops.dto.transaction.ResponseTransactionDTO;
+import com.robertoljr.sops.entity.Transaction;
+import com.robertoljr.sops.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+@Mapper(componentModel = "spring", uses = {UserMapper.class})
+public interface TransactionMapper {
+
+    // CreateTransactionDTO -> Transaction
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "sender", source = "senderId", qualifiedByName = "fromId")
+    @Mapping(target = "recipient", source = "recipientId", qualifiedByName = "fromId")
+    Transaction toEntity(CreateTransactionDTO dto);
+
+    // Transaction -> ResponseTransactionDTO
+    ResponseTransactionDTO toResponseDTO(Transaction transaction);
+
+    @Named("fromId")
+    default User fromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+
+        User user = new User();
+        user.setId(id);
+
+        return user;
+    }
+}
